c26b62edb43a41a5e5220b75ed7c1a667ae4c103
GuisoPiu <gruposiice.hr@gmail.com>
2022-06-10 19:08:37 -0500

    JobDetails component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus deserunt nulla laudantium saepe consequatur! Praesentium ipsum corporis ratione consequatur aliquam. Corporis ullam sit voluptates laudantium fugiat in facere ab porro quaerat autem! Dolorum velit autem dolorem, perferendis suscipit laudantium mollitia natus, quod amet iusto, quisquam quae. Inventore quibusdam alias commodi! Corrupti doloribus magnam ipsum officia cumque iure culpa suscipit voluptate velit vitae ea, error cupiditate veritatis amet cum aspernatur itaque officiis deleniti voluptatem iste eveniet ullam. Dicta sit accusamus fugit eaque corporis explicabo voluptatum cum vitae natus, officia cumque beatae modi, quidem perspiciatis excepturi eos? Ducimus eos debitis nam deserunt.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe expedita repudiandae consequatur unde quod ipsum labore doloribus totam iure qui! Facere quis odit eligendi, alias eaque reiciendis iste, esse, incidunt iusto consequatur culpa atque quos dolorem a quisquam nostrum voluptatum ratione odio perferendis. Tempore debitis et unde at tempora harum doloribus, voluptate repudiandae maiores rem! Earum, ea quia fugit animi reprehenderit voluptate? Non quia labore delectus consequuntur, atque aliquid, tenetur unde dolores eveniet provident earum eligendi amet, ipsum consequatur impedit? Ducimus a temporibus illum fuga harum, ut quasi. Saepe facere voluptates placeat explicabo maxime facilis omnis sapiente excepturi quae corrupti?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe expedita repudiandae consequatur unde quod ipsum labore doloribus totam iure qui! Facere quis odit eligendi, alias eaque reiciendis iste, esse, incidunt iusto consequatur culpa atque quos dolorem a quisquam nostrum voluptatum ratione odio perferendis. Tempore debitis et unde at tempora harum doloribus, voluptate repudiandae maiores rem! Earum, ea quia fugit animi reprehenderit voluptate? Non quia labore delectus consequuntur, atque aliquid, tenetur unde dolores eveniet provident earum eligendi amet, ipsum consequatur impedit? Ducimus a temporibus illum fuga harum, ut quasi. Saepe facere voluptates placeat explicabo maxime facilis omnis sapiente excepturi quae corrupti?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
